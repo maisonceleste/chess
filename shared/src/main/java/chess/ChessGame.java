d16e7fac79cc9ff1b0756744ef2e3e1b439b2cf6
maisonceleste <maisonceleste@gmail.com>
2025-05-10 23:18:19 -0600

    team turn getter and setter

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -21,7 +21,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return teamTurn;
     }
 
     /**
@@ -29,9 +29,7 @@ public class ChessGame {
      *
      * @param team the team whose turn it is
      */
-    public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
-    }
+    public void setTeamTurn(TeamColor team) {teamTurn=team;}
 
     /**
      * Enum identifying the 2 possible teams in a chess game
