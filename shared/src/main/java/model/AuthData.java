296bc2602386b0a18fb8acf15eed9f30877cacb3
maisonceleste <maisonceleste@gmail.com>
2025-05-20 22:54:17 -0600

    idek

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -2,33 +2,4 @@ package model;
 import java.util.Objects;
 import java.util.UUID;
 
-public class AuthData {
-    private final String authToken;
-    private final String username;
-
-    public AuthData(String username){
-        this.username=username;
-        this.authToken = UUID.randomUUID().toString();
-    }
-
-    public String getAuthToken() {
-        return authToken;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof AuthData authData)) {
-            return false;
-        }
-        return Objects.equals(authToken, authData.authToken) && Objects.equals(username, authData.username);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(authToken, username);
-    }
-}
+public record AuthData (String authToken, String username){}
--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -2,39 +2,4 @@ package model;
 
 import java.util.Objects;
 
-public class UserData {
-    private final String username;
-    private final String password;
-    private final String email;
-
-    public UserData(String username, String password, String email){
-        this.username=username;
-        this.password=password;
-        this.email=email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public boolean comparePassword(String comp) {
-        return (Objects.equals(this.password, comp));
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof UserData userData)) {
-            return false;
-        }
-        return Objects.equals(username, userData.username) && Objects.equals(password, userData.password) && Objects.equals(email, userData.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(username, password, email);
-    }
-}
+public record UserData(String username, String password, String email) {}
