7275db1bb31f8807164464bcdcc749db2dd718da
maisonceleste <maisonceleste@gmail.com>
2025-05-28 23:40:06 -0600

    DataAccess unit test

---
+++ server/src/test/java/dataaccess/MySqlDataAccessUnitTest.java
@@ -0,0 +1,142 @@
+package dataaccess;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import responseexception.ResponseException;
+import service.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MySqlDataAccessUnitTest {
+    static final ChessService SERVICE;
+
+    static {
+        try {
+            SERVICE = new ChessService(new MySqlDataAccess());
+        } catch (ResponseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeEach
+    void clear() throws ResponseException {
+        SERVICE.dataAccess.deleteAll();
+        SERVICE.dataAccess.resetGameID();
+    }
+
+    @Test
+    void registerUser() throws ResponseException {
+        RegisterResult result = SERVICE.register("correctUsername", "correctPassword", "correctEmail");
+
+        RegisterResult expectedResult = new RegisterResult("correctUsername", "random auth code");
+
+        assertEquals(expectedResult.username(), result.username());
+        assertNotNull(result.authToken());
+    }
+
+    @Test
+    void registerBadUser() throws ResponseException {
+        SERVICE.register("correctUsername", "correctPassword", "correctEmail");
+        assertThrows(ResponseException.class, () -> SERVICE.register("correctUsername", "evil", "Evil"));
+        assertThrows(ResponseException.class, () -> SERVICE.register(null, "evil", "superEvil"));
+    }
+
+    @Test
+    void loginUser() throws ResponseException{
+        SERVICE.register("correctUsername", "correctPassword", "correctEmail");
+        LoginResult result = SERVICE.login("correctUsername", "correctPassword");
+
+        LoginResult expectedResult = new LoginResult("correctUsername", "random auth token");
+
+        assertEquals(expectedResult.username(), result.username());
+        assertNotNull(result.authToken());
+    }
+
+    @Test
+    void loginBadUser() throws ResponseException{
+        SERVICE.register("correctUsername", "correctPassword", "correctEmail");
+        assertThrows(ResponseException.class, () -> SERVICE.login("correctUsername", "evil"));
+        assertThrows(ResponseException.class, () -> SERVICE.login(null, "correctPassword"));
+    }
+
+    @Test
+    void logoutUser() throws ResponseException{
+        RegisterResult registration = SERVICE.register("correctUsername", "correctPassword", "correctEmail");
+        String authToken = registration.authToken();
+        SERVICE.logout(authToken);
+        assertNull(SERVICE.dataAccess.getAuth(authToken));
+    }
+
+    @Test
+    void logoutBadUser() throws ResponseException{
+        RegisterResult registration = SERVICE.register("correctUsername", "correctPassword", "correctEmail");
+        assertThrows(ResponseException.class, () -> SERVICE.logout("FALSE authToken"));
+    }
+
+    @Test
+    void createGame() throws ResponseException{
+        RegisterResult registration = SERVICE.register("correctUsername", "correctPassword", "correctEmail");
+        String authToken = registration.authToken();
+        CreateResult result = SERVICE.create(authToken, "testGame");
+        assertEquals(1, result.gameID());
+    }
+
+    @Test
+    void createBadGame() throws ResponseException{
+        RegisterResult registration = SERVICE.register("correctUsername", "correctPassword", "correctEmail");
+        String authToken = registration.authToken();
+        assertThrows(ResponseException.class, () -> SERVICE.create("FALSE authToken", "testGame"));
+        assertThrows(ResponseException.class, () -> SERVICE.create(authToken , null));
+    }
+
+    @Test
+    void listGames() throws ResponseException {
+        RegisterResult registration = SERVICE.register("correctUsername", "correctPassword", "correctEmail");
+        String authToken = registration.authToken();
+        SERVICE.create(authToken, "testGame1");
+        SERVICE.create(authToken, "testGame2");
+        ListResult result = SERVICE.list(authToken);
+        assertEquals(2, result.games().size());
+    }
+
+    @Test
+    void badListGames() throws ResponseException {
+        RegisterResult registration = SERVICE.register("correctUsername", "correctPassword", "correctEmail");
+        String authToken = registration.authToken();
+        SERVICE.create(authToken, "testGame1");
+        SERVICE.create(authToken, "testGame2");
+        assertThrows(ResponseException.class, () -> SERVICE.list("bad auth token"));
+    }
+
+    @Test
+    void joinGame() throws ResponseException {
+        RegisterResult registration = SERVICE.register("correctUsername", "correctPassword", "correctEmail");
+        String authToken = registration.authToken();
+        SERVICE.create(authToken, "testGame1");
+        assert(SERVICE.join("WHITE", 1, authToken));
+    }
+
+    @Test
+    void badJoinGame() throws ResponseException {
+        RegisterResult registration = SERVICE.register("correctUsername", "correctPassword", "correctEmail");
+        String authToken = registration.authToken();
+        SERVICE.create(authToken, "testGame1");
+        SERVICE.join("WHITE", 1, authToken);
+        assertThrows(ResponseException.class, () -> SERVICE.join("WHITE", 1, authToken));
+    }
+
+    @Test
+    void testClear() throws ResponseException {
+        RegisterResult registration = SERVICE.register("correctUsername", "correctPassword", "correctEmail");
+        String authToken = registration.authToken();
+        SERVICE.create(authToken, "testGame1");
+        SERVICE.clear();
+        assertNull(SERVICE.dataAccess.getUser("correctUsername"));
+        assertNull(SERVICE.dataAccess.getAuth(authToken));
+        assertNull(SERVICE.dataAccess.getGame(1));
+    }
+}
