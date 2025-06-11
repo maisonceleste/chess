5de88716859878294a89a3f7b0c1eff54d74d5bb
maisonceleste <maisonceleste@gmail.com>
2025-06-10 21:53:08 -0600

    Initial class implementations

---
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -0,0 +1,22 @@
+package server.websocket;
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+import java.io.IOException;
+
+@WebSocket
+public class WebSocketHandler {
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        Action action = new Gson().fromJson(message, Action.class);
+        switch (action.type()) {
+            case ENTER -> enter(action.visitorName(), session);
+            case EXIT -> exit(action.visitorName());
+        }
+    }
+}
